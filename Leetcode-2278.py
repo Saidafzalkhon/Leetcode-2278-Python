3241171a040d6cc8d0011f4d2e3a630eef9b3afd
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2022-07-04 15:30:45 +0500

    Add commit

--- Leetcode-2278.py
+++ Leetcode-2278.py
@@ -1,3 +1,9 @@
-s = input()
-letter = input()
-print(int(s.count(letter)/len(s)*100))
+num = list(map(int,input().split()))
+k = int(input())
+s = ''
+for i in num:
+    s+=str(i)
+s = int(s)+k
+num.clear()
+a = list(str(s))
+print(a)
